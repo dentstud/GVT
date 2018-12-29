a6744aa49f1746b03e9e96e0ce4463e15cc640b4
dentstud <46047939+dentstud@users.noreply.github.com>
2018-12-30 02:12:06 +1100

    Add files via upload

---
+++ gvt-heroku.py
@@ -0,0 +1,583 @@
+
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+import pymongo
+
+
+
+client = Client("a","b")
+
+bm = BinanceSocketManager(client)
+
+
+    # collection is mongo is same as a Table 
+
+
+myclient_1 = pymongo.MongoClient("mongodb://poya1:Muhammad00.@ds243254.mlab.com:43254/gvtbtc")
+mydb_1 = myclient_1 ["gvtbtc"]
+mycol_1 = mydb_1 ["data"] 
+
+
+myclient_2 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds245234.mlab.com:45234/mdabtc")
+mydb_2 = myclient_2 ["mdabtc"]
+mycol_2 = mydb_2 ["data"]
+ 
+
+
+myclient_3 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds243254.mlab.com:43254/eosbtc")
+mydb_3 = myclient_3 ["eosbtc"]
+mycol_3 = mydb_3 ["data"] 
+
+myclient_4 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds243254.mlab.com:43254/adabtc")
+mydb_4 = myclient_4 ["adabtc"]
+mycol_4 = mydb_4 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_5 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds245234.mlab.com:45234/dcrbtc")
+mydb_5 = myclient_5 ["dcrbtc"]
+mycol_5 = mydb_5 ["data"]
+ 
+myclient_6 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/bqxbtc")
+mydb_6 = myclient_6 ["bqxbtc"]
+mycol_6 = mydb_6 ["data"] 
+
+myclient_7 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds245234.mlab.com:45234/dashbtc")
+mydb_7 = myclient_7 ["dashbtc"]
+mycol_7 = mydb_7 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_8 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds245234.mlab.com:45234/icxbtc")
+mydb_8 = myclient_8 ["icxbtc"]
+mycol_8 = mydb_8 ["data"]
+ 
+myclient_9 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/iotabtc")
+mydb_9 = myclient_9 ["iotabtc"]
+mycol_9 = mydb_9 ["data"] 
+
+myclient_10 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds245234.mlab.com:45234/kmdbtc")
+mydb_10 = myclient_10 ["kmdbtc"]
+mycol_10 = mydb_10 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_11 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/manabtc")
+mydb_11 = myclient_11 ["manabtc"]
+mycol_11 = mydb_11 ["data"]
+ 
+myclient_12 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/mtlbtc")
+mydb_12 = myclient_12 ["mtlbtc"]
+mycol_12 = mydb_12 ["data"] 
+
+
+myclient_13 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/ontbtc")
+mydb_13 = myclient_13 ["ontbtc"]
+mycol_13 = mydb_13 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_14 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/qkcbtc")
+mydb_14 = myclient_14 ["qkcbtc"]
+mycol_14 = mydb_14 ["data"]
+ 
+myclient_15 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/rvnbtc")
+mydb_15 = myclient_15 ["rvnbtc"]
+mycol_15 = mydb_15 ["data"] 
+
+myclient_16 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/thetabtc")
+mydb_16 = myclient_16 ["thetabtc"]
+mycol_16 = mydb_16 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_17 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/xmrbtc")
+mydb_17 = myclient_17 ["xmrbtc"]
+mycol_17 = mydb_17 ["data"]
+ 
+myclient_18 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds245234.mlab.com:45234/zecbtc")
+mydb_18 = myclient_18 ["zecbtc"]
+mycol_18 = mydb_18 ["data"] 
+
+myclient_19 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/zrxbtc")
+mydb_19 = myclient_19 ["zrxbtc"]
+mycol_19 = mydb_19 ["data"]     # collection is mongo is same as a Table 
+
+
+#------------------------------------------------------------------
+
+
+
+myclient_20 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds119161.mlab.com:19161/batbtc")
+mydb_20 = myclient_20 ["batbtc"]
+mycol_20 = mydb_20 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_21 = pymongo.MongoClient("mongodb://poya1:Muhammad00.@ds119161.mlab.com:19161/hotbtc")
+mydb_21 = myclient_21 ["hotbtc"]
+mycol_21 = mydb_21 ["data"] 
+
+
+myclient_22 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds119161.mlab.com:19161/nanobtc")
+mydb_22 = myclient_22 ["nanobtc"]
+mycol_22 = mydb_22 ["data"]
+ 
+
+
+myclient_23 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/linkbtc")
+mydb_23 = myclient_23 ["linkbtc"]
+mycol_23 = mydb_23 ["data"] 
+
+myclient_24 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/vibbtc")
+mydb_24 = myclient_24 ["vibbtc"]
+mycol_24 = mydb_24 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_25 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/wanbtc")
+mydb_25 = myclient_25 ["wanbtc"]
+mycol_25 = mydb_25 ["data"]
+ 
+myclient_26 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds239128.mlab.com:39128/aionbtc")
+mydb_26 = myclient_26 ["aionbtc"]
+mycol_26 = mydb_26 ["data"] 
+
+myclient_27 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/mithbtc")
+mydb_27 = myclient_27 ["mithbtc"]
+mycol_27 = mydb_27 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_28 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/xembtc")
+mydb_28 = myclient_28 ["xembtc"]
+mycol_28 = mydb_28 ["data"]
+ 
+myclient_29 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/qtumbtc")
+mydb_29 = myclient_29 ["qtumbtc"]
+mycol_29 = mydb_29 ["data"] 
+
+myclient_30 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds239128.mlab.com:39128/repbtc")
+mydb_30 = myclient_30 ["repbtc"]
+mycol_30 = mydb_30 ["data"]     # collection is mongo is same as a Table 
+
+
+
+myclient_31 = pymongo.MongoClient("mongodb://poya1:Muhammad00.@ds127589.mlab.com:27589/xvgbtc")
+mydb_31 = myclient_31 ["xvgbtc"]
+mycol_31 = mydb_31 ["data"] 
+
+
+myclient_32 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/iostbtc")
+mydb_32 = myclient_32 ["iostbtc"]
+mycol_32 = mydb_32 ["data"]
+ 
+
+
+myclient_33 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds145304.mlab.com:45304/qkcbtc")
+mydb_33 = myclient_33 ["qkcbtc"]
+mycol_33 = mydb_33 ["data"] 
+
+myclient_34 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/snmbtc")
+mydb_34 = myclient_34 ["snmbtc"]
+mycol_34 = mydb_34 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_35 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/wtcbtc")
+mydb_35 = myclient_35 ["wtcbtc"]
+mycol_35 = mydb_35 ["data"]
+ 
+myclient_36 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds127589.mlab.com:27589/omgbtc")
+mydb_36 = myclient_36 ["omgbtc"]
+mycol_36 = mydb_36 ["data"] 
+
+myclient_37 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds037185.mlab.com:37185/arnbtc")
+mydb_37 = myclient_37 ["arnbtc"]
+mycol_37 = mydb_37 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_38 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds233571.mlab.com:33571/dgdbtc")
+mydb_38 = myclient_38 ["dgdbtc"]
+mycol_38 = mydb_38 ["data"]
+ 
+myclient_39 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds225253.mlab.com:25253/databtc")
+mydb_39 = myclient_39 ["databtc"]
+mycol_39 = mydb_39 ["data"] 
+
+myclient_40 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds225253.mlab.com:25253/pivxbtc")
+mydb_40 = myclient_40 ["pivxbtc"]
+mycol_40 = mydb_40 ["data"]     # collection is mongo is same as a Table 
+
+
+
+myclient_41 = pymongo.MongoClient("mongodb://poya1:Muhammad00.@ds163672.mlab.com:63672/mftbtc")
+mydb_41 = myclient_41 ["mftbtc"]
+mycol_41 = mydb_41 ["data"] 
+
+
+myclient_42 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds225253.mlab.com:25253/lskbtc")
+mydb_42 = myclient_42 ["lskbtc"]
+mycol_42 = mydb_42 ["data"]
+ 
+
+
+myclient_43 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds233571.mlab.com:33571/cmtbtc")
+mydb_43 = myclient_43 ["cmtbtc"]
+mycol_43 = mydb_43 ["data"] 
+
+myclient_44 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds233571.mlab.com:33571/insbtc")
+mydb_44 = myclient_44 ["insbtc"]
+mycol_44 = mydb_44 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_45 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds233571.mlab.com:33571/qlcbtc")
+mydb_45 = myclient_45 ["qlcbtc"]
+mycol_45 = mydb_45 ["data"]
+ 
+myclient_46 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds233571.mlab.com:33571/enjbtc")
+mydb_46 = myclient_46 ["enjbtc"]
+mycol_46 = mydb_46 ["data"] 
+
+myclient_47 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds017193.mlab.com:17193/polybtc")
+mydb_47 = myclient_47 ["polybtc"]
+mycol_47 = mydb_47 ["data"]     # collection is mongo is same as a Table 
+
+
+myclient_48 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds051524.mlab.com:51524/vibebtc")
+mydb_48 = myclient_48 ["vibebtc"]
+mycol_48 = mydb_48 ["data"]
+ 
+myclient_49 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds029831.mlab.com:29831/gobtc")
+mydb_49 = myclient_49 ["gobtc"]
+mycol_49 = mydb_49 ["data"] 
+
+myclient_50 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds125871.mlab.com:25871/neblbtc")
+mydb_50 = myclient_50 ["neblbtc"]
+mycol_50 = mydb_50 ["data"]     # collection is mongo is same as a Table 
+
+myclient_51 = pymongo.MongoClient("mongodb://poya:Muhammad00.@ds153659.mlab.com:53659/gntbtc")
+mydb_51 = myclient_51 ["gntbtc"]
+mycol_51 = mydb_51 ["data"]     # collection is mongo is same as a Table 
+
+
+
+
+
+##############################################################################
+##############################################################################
+
+def process_message1(a1):
+    mydict_1 = [a1]
+    mycol_1.insert_many(mydict_1)
+    print(a1['k']['c'])
+
+def process_message2(a2):
+    mydict_2 = [a2]
+    mycol_2.insert_many(mydict_2)
+    print(a2['k']['c'])
+
+def process_message3(a3):
+    mydict_3 = [a3]
+    mycol_3.insert_many(mydict_3)
+    print(a3['k']['c'])
+
+def process_message4(a4):
+    mydict_4 = [a4]
+    mycol_4.insert_many(mydict_4)
+    print(a4['k']['c'])
+
+def process_message5(a5):
+    mydict_5 = [a5]
+    mycol_5.insert_many(mydict_5)
+    print(a5['k']['c'])
+
+def process_message6(a6):
+    mydict_6 = [a6]
+    mycol_6.insert_many(mydict_6)
+    print(a6['k']['c'])
+
+def process_message7(a7):
+    mydict_7 = [a7]
+    mycol_7.insert_many(mydict_7)
+    print(a7['k']['c'])
+
+def process_message8(a8):
+    mydict_8 = [a8]
+    mycol_8.insert_many(mydict_8)
+    print(a8['k']['c'])
+
+def process_message9(a9):
+    mydict_9 = [a9]
+    mycol_9.insert_many(mydict_9)
+    print(a9['k']['c'])
+
+def process_message10(a10):
+    mydict_10 = [a10]
+    mycol_10.insert_many(mydict_10)
+    print(a10['k']['c'])
+
+def process_message11(a11):
+    mydict_11 = [a11]
+    mycol_11.insert_many(mydict_11)
+    print(a11['k']['c'])
+
+def process_message12(a12):
+    mydict_12 = [a12]
+    mycol_12.insert_many(mydict_12)
+    print(a12['k']['c'])
+
+def process_message13(a13):
+    mydict_13 = [a13]
+    mycol_13.insert_many(mydict_13)
+    print(a13['k']['c'])
+
+def process_message14(a14):
+    mydict_14 = [a14]
+    mycol_14.insert_many(mydict_14)
+    print(a14['k']['c'])
+
+def process_message15(a15):
+    mydict_15 = [a15]
+    mycol_15.insert_many(mydict_15)
+    print(a15['k']['c'])
+
+def process_message16(a16):
+    mydict_16 = [a16]
+    mycol_16.insert_many(mydict_16)
+    print(a16['k']['c'])
+
+def process_message17(a17):
+    mydict_17 = [a17]
+    mycol_17.insert_many(mydict_17)
+    print(a17['k']['c'])
+
+def process_message18(a18):
+    mydict_18 = [a18]
+    mycol_18.insert_many(mydict_18)
+    print(a18['k']['c'])
+
+def process_message19(a19):
+    mydict_19 = [a19]
+    mycol_19.insert_many(mydict_19)
+    print(a19['k']['c'])
+
+#### SECOND STAGE  ##################
+
+def process_message20(a20):
+    mydict_20 = [a20]
+    mycol_20.insert_many(mydict_20)
+    print(a20['k']['c'])
+
+def process_message21(a21):
+    mydict_21 = [a21]
+    mycol_21.insert_many(mydict_21)
+    print(a21['k']['c'])
+
+def process_message22(a22):
+    mydict_22 = [a22]
+    mycol_22.insert_many(mydict_22)
+    print(a22['k']['c'])
+
+def process_message23(a23):
+    mydict_23 = [a23]
+    mycol_23.insert_many(mydict_23)
+    print(a23['k']['c'])
+
+def process_message24(a24):
+    mydict_24 = [a24]
+    mycol_24.insert_many(mydict_24)
+    print(a24['k']['c'])
+
+def process_message25(a25):
+    mydict_25 = [a25]
+    mycol_25.insert_many(mydict_25)
+    print(a25['k']['c'])
+
+def process_message26(a26):
+    mydict_26 = [a26]
+    mycol_26.insert_many(mydict_26)
+    print(a26['k']['c'])
+
+def process_message27(a27):
+    mydict_27 = [a27]
+    mycol_27.insert_many(mydict_27)
+    print(a27['k']['c'])
+
+def process_message28(a28):
+    mydict_28 = [a28]
+    mycol_28.insert_many(mydict_28)
+    print(a28['k']['c'])
+
+def process_message29(a29):
+    mydict_29 = [a29]
+    mycol_29.insert_many(mydict_29)
+    print(a29['k']['c'])
+
+def process_message30(a30):
+    mydict_30 = [a30]
+    mycol_30.insert_many(mydict_30)
+    print(a30['k']['c'])
+
+
+def process_message31(a31):
+    mydict_31 = [a31]
+    mycol_31.insert_many(mydict_31)
+    print(a31['k']['c'])
+
+def process_message32(a32):
+    mydict_32 = [a32]
+    mycol_32.insert_many(mydict_32)
+    print(a32['k']['c'])
+
+def process_message33(a33):
+    mydict_33 = [a33]
+    mycol_33.insert_many(mydict_33)
+    print(a33['k']['c'])
+
+def process_message34(a34):
+    mydict_34 = [a34]
+    mycol_34.insert_many(mydict_34)
+    print(a34['k']['c'])
+
+def process_message35(a35):
+    mydict_35 = [a35]
+    mycol_35.insert_many(mydict_35)
+    print(a35['k']['c'])
+
+def process_message36(a36):
+    mydict_36 = [a36]
+    mycol_36.insert_many(mydict_36)
+    print(a36['k']['c'])
+
+def process_message37(a37):
+    mydict_37 = [a37]
+    mycol_37.insert_many(mydict_37)
+    print(a37['k']['c'])
+
+def process_message38(a38):
+    mydict_38 = [a38]
+    mycol_38.insert_many(mydict_38)
+    print(a38['k']['c'])
+
+def process_message39(a39):
+    mydict_39 = [a39]
+    mycol_39.insert_many(mydict_39)
+    print(a39['k']['c'])
+
+def process_message40(a40):
+    mydict_40 = [a40]
+    mycol_40.insert_many(mydict_40)
+    print(a40['k']['c'])
+    
+
+def process_message41(a41):
+    mydict_41 = [a41]
+    mycol_41.insert_many(mydict_41)
+    print(a41['k']['c'])
+
+def process_message42(a42):
+    mydict_42 = [a42]
+    mycol_42.insert_many(mydict_42)
+    print(a2['k']['c'])
+
+def process_message43(a43):
+    mydict_43 = [a43]
+    mycol_43.insert_many(mydict_43)
+    print(a43['k']['c'])
+
+def process_message44(a44):
+    mydict_44 = [a44]
+    mycol_44.insert_many(mydict_44)
+    print(a44['k']['c'])
+
+def process_message45(a45):
+    mydict_45 = [a45]
+    mycol_45.insert_many(mydict_45)
+    print(a45['k']['c'])
+
+def process_message46(a46):
+    mydict_46 = [a46]
+    mycol_46.insert_many(mydict_46)
+    print(a46['k']['c'])
+
+def process_message47(a47):
+    mydict_47 = [a47]
+    mycol_47.insert_many(mydict_47)
+    print(a47['k']['c'])
+
+def process_message48(a48):
+    mydict_48 = [a48]
+    mycol_48.insert_many(mydict_48)
+    print(a48['k']['c'])
+
+def process_message49(a49):
+    mydict_49 = [a49]
+    mycol_49.insert_many(mydict_49)
+    print(a49['k']['c'])
+
+def process_message50(a50):
+    mydict_50 = [a50]
+    mycol_50.insert_many(mydict_50)
+    print(a50['k']['c'])
+
+def process_message51(a51):
+    mydict_51 = [a51]
+    mycol_51.insert_many(mydict_51)
+    print(a51['k']['c'])
+
+
+# 
+    
+    
+
+conn_key = bm.start_kline_socket('GVTBTC', process_message1, interval='15m')
+conn_key = bm.start_kline_socket('MDABTC', process_message2, interval = '15m')
+conn_key = bm.start_kline_socket('EOSBTC', process_message3, interval='15m')
+conn_key = bm.start_kline_socket('ADABTC', process_message4, interval = '15m')
+conn_key = bm.start_kline_socket('DCRBTC', process_message5, interval = '15m')
+conn_key = bm.start_kline_socket('BQXBTC', process_message6, interval='15m')
+conn_key = bm.start_kline_socket('DASHBTC', process_message7, interval = '15m')
+conn_key = bm.start_kline_socket('ICXBTC', process_message8, interval='15m')
+conn_key = bm.start_kline_socket('IOTABTC', process_message9, interval = '15m')
+conn_key = bm.start_kline_socket('KMDBTC', process_message10, interval='15m')
+conn_key = bm.start_kline_socket('MANABTC', process_message11, interval = '15m')
+conn_key = bm.start_kline_socket('MTLBTC', process_message12, interval='15m')
+conn_key = bm.start_kline_socket('ONTBTC', process_message13, interval = '15m')
+conn_key = bm.start_kline_socket('QKCBTC', process_message14, interval='15m')
+conn_key = bm.start_kline_socket('RVNBTC', process_message15, interval = '15m')
+conn_key = bm.start_kline_socket('THETABTC', process_message16, interval='15m')
+conn_key = bm.start_kline_socket('XMRBTC', process_message17, interval = '15m')
+conn_key = bm.start_kline_socket('ZECBTC', process_message18, interval='15m')
+conn_key = bm.start_kline_socket('ZRXBTC', process_message19, interval = '15m')
+
+
+## second stage  ##
+
+conn_key = bm.start_kline_socket('BATBTC', process_message20, interval='15m')
+conn_key = bm.start_kline_socket('HOTBTC', process_message21, interval = '15m')
+conn_key = bm.start_kline_socket('NANOBTC', process_message22, interval='15m')
+conn_key = bm.start_kline_socket('LINKBTC', process_message23, interval = '15m')
+conn_key = bm.start_kline_socket('VIBBTC', process_message24, interval = '15m')
+conn_key = bm.start_kline_socket('WANBTC', process_message25, interval='15m')
+conn_key = bm.start_kline_socket('AIONBTC', process_message26, interval = '15m')
+conn_key = bm.start_kline_socket('MITHBTC', process_message27, interval='15m')
+conn_key = bm.start_kline_socket('XEMBTC', process_message28, interval = '15m')
+conn_key = bm.start_kline_socket('QTUMBTC', process_message29, interval='15m')
+conn_key = bm.start_kline_socket('REPBTC', process_message30, interval = '15m')
+conn_key = bm.start_kline_socket('XVGBTC', process_message31, interval='15m')
+conn_key = bm.start_kline_socket('IOSTBTC', process_message32, interval = '15m')
+conn_key = bm.start_kline_socket('QKCBTC', process_message33, interval='15m')
+conn_key = bm.start_kline_socket('SNMBTC', process_message34, interval = '15m')
+conn_key = bm.start_kline_socket('WTCBTC', process_message35, interval='15m')
+conn_key = bm.start_kline_socket('OMGBTC', process_message36, interval = '15m')
+conn_key = bm.start_kline_socket('ARNBTC', process_message37, interval='15m')
+conn_key = bm.start_kline_socket('DGDBTC', process_message38, interval = '15m')
+conn_key = bm.start_kline_socket('DATABTC', process_message39, interval='15m')
+conn_key = bm.start_kline_socket('PIVXBTC', process_message40, interval = '15m')
+conn_key = bm.start_kline_socket('MFTBTC', process_message41, interval='15m')
+conn_key = bm.start_kline_socket('LSKBTC', process_message42, interval = '15m')
+conn_key = bm.start_kline_socket('CMTBTC', process_message43, interval = '15m')
+conn_key = bm.start_kline_socket('INSBTC', process_message44, interval='15m')
+conn_key = bm.start_kline_socket('QLCBTC', process_message45, interval = '15m')
+conn_key = bm.start_kline_socket('ENJBTC', process_message46, interval='15m')
+conn_key = bm.start_kline_socket('POLYBTC', process_message47, interval = '15m')
+conn_key = bm.start_kline_socket('VIBEBTC', process_message48, interval='15m')
+conn_key = bm.start_kline_socket('GOBTC', process_message49, interval = '15m')
+conn_key = bm.start_kline_socket('NEBLBTC', process_message50, interval='15m')
+conn_key = bm.start_kline_socket('GNTBTC', process_message51, interval = '15m')
+
+
+
+bm.start()
